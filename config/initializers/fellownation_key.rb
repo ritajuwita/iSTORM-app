22c11d64c711172c5f5df9f21b58b6ca5b1e88ba
rita juwita <rita.j@kiranatama.com>
2011-05-02 15:59:44 +0700

    Updating key and secret key

--- config/initializers/fellownation_key.rb
+++ config/initializers/fellownation_key.rb
@@ -7,15 +7,15 @@ if RAILS_ENV == 'development' || RAILS_ENV == 'test' || RAILS_ENV == 'cucumber'
   WEBSERVIUS_KEY = "Dbsv8wmfax2J5xzAK7_KZ7qrXIiXWR1j"
 elsif RAILS_ENV == 'staging'
   SITE = 'http://staging.fellownation.com'
-  FKEY = "LsFhkv0Ukc8fBI9lwbg"
-  FSECRET = "htFCtczsru7mRArtlIrX6Wbazhbx1YlrD0pIhwMw"
+  FKEY = "MZqY1iE9DruYDwZXYDf9"
+  FSECRET = "p53EnAjEcIZ_5d4hAtwhe4wjKsTH0fUG"
   IFRAME_HELPER = "http://apps.staging.fellownation.com/iframe_helper.html"
   WEBSERVIUS_PATH = "http://api.staging.fellownation.com"
   WEBSERVIUS_KEY = "mGvdSViQphKO_m-WB9Ed3pHcXgW_N5cX"
 else
   SITE = 'http://fellownation.com'
-  FKEY = "MZqY1iE9DruYDwZXYDf9"
-  FSECRET = "p53EnAjEcIZ_5d4hAtwhe4wjKsTH0fUG"
+  FKEY = "LsFhkv0Ukc8fBI9lwbg"
+  FSECRET = "htFCtczsru7mRArtlIrX6Wbazhbx1YlrD0pIhwMw"
   IFRAME_HELPER = "http://apps.fellownation.com/iframe_helper.html"
   WEBSERVIUS_PATH = "http://api.fellownation.com"
   WEBSERVIUS_KEY = "310LphNaOkbYd2-gcmyNemo-Dl5LGZ-g"
